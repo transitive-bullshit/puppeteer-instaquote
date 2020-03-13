a91a6ff7e2a31d0b022bd310e1573d8f391cbf22
Travis Fischer <fisch0920@gmail.com>
2020-03-13 02:20:37 -0400

    🌦

--- index.js
+++ index.js
@@ -7,30 +7,35 @@ const { getImage } = require('./lib/get-image')
 const { getFonts } = require('./lib/get-fonts')
 
 /**
- * Renders the given meme via puppeteer.
- *
- * Asynchronously returns the generated html page as a string for debugging purposes.
+ * Renders the given meme via puppeteer to an `output` file.
  *
  * If you want to load multiple google fonts, juse specify their font-families in `opts.style.fontFamily`
  * separated by commas as you normally would for CSS fonts.
  *
- * @name renderText
+ * The generated html page is returned as a string for debugging and batch rendering purposes.
+ *
+ * @name renderInstaquote
  * @function
  *
  * @param {object} opts - Configuration options
  * @param {string} opts.output - Path of image file to store result
- * @param {number} [opts.width] - Optional max width for word-wrap
- * @param {number} [opts.height] - Optional max height to clip overflow
- * @param {object} [opts.style={}] - JS [CSS styles](https://www.w3schools.com/jsref/dom_obj_style.asp) to apply to the text's container div
+ * @param {string} opts.quote - Main text (or html) of the content to render.
+ * @param {string} opts.author - Main text (or html) of the sub-header (content author).
+ * @param {string} [opts.background] - Background image either as a local file path or as a URL (defaults to a transparent background).
+ * @param {number} [opts.width=1080] - Optional width of output image
+ * @param {number} [opts.height=1080] - Optional height of output image
+ * @param {object} [opts.style={}] - Top-level [CSS styles](https://www.w3schools.com/jsref/dom_obj_style.asp) to apply to the top-level container div.
+ * @param {object} [opts.quoteStyle={}] - Quote / main content [CSS styles](https://www.w3schools.com/jsref/dom_obj_style.asp) to apply to the main content div.
+ * @param {object} [opts.authorStyle={}] - Author [CSS styles](https://www.w3schools.com/jsref/dom_obj_style.asp) to apply to the author's div.
  *
  * @return {Promise}
  */
 module.exports = async (opts) => {
   const {
+    output,
     quote,
     author,
     background,
-    output,
     width = 1080,
     height = 1080,
     style = {},
@@ -136,6 +141,7 @@ body {
     const quote = document.getElementById('quote');
     const author = document.getElementById('author');
 
+    // Find the optimal font size to maximize the available layout area without overflow
     do {
       quote.style.fontSize = f + 'px';
       author.style.fontSize = (f * 0.5 | 0) + 'px';
